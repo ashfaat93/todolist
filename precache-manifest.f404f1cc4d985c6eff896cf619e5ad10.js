a3a60db2117fd2f361290cb02856eddb58f2eac7
Ali <ashfaat@gmail.com>
2020-08-11 11:56:23 +0100

    Updates

---
+++ precache-manifest.f404f1cc4d985c6eff896cf619e5ad10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9851638488294cabc9c5723616b3c3a9",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5fd9053aeb671149ca44",
+    "url": "/todolist/static/css/main.123288b7.chunk.css"
+  },
+  {
+    "revision": "cc61aa37f07362297dc9",
+    "url": "/todolist/static/js/2.412d8d2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.412d8d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd9053aeb671149ca44",
+    "url": "/todolist/static/js/main.264a25d1.chunk.js"
+  },
+  {
+    "revision": "08a965f83a22e8ac2de9",
+    "url": "/todolist/static/js/runtime-main.1b1a5ba3.js"
+  }
+]);
